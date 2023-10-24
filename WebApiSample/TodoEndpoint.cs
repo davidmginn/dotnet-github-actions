6374aa3bbb74dedfc9253853f5b526747a9ea965
David Ginn <davidmginn@gmail.com>
2023-10-24 13:13:36 -0400

    adds commented endpoint

--- WebApiSample/TodoEndpoint.cs
+++ WebApiSample/TodoEndpoint.cs
@@ -8,6 +8,7 @@ public static class TodoEndpoint
     public static RouteGroupBuilder MapTodoApi(this RouteGroupBuilder routeGroupBuilder)
     {
         routeGroupBuilder.MapGet("/", GetAllTodos);
+        //routeGroupBuilder.MapGet("/{id:int}", GetTodoById);
 
         return routeGroupBuilder;
     }
@@ -23,4 +24,24 @@ public static class TodoEndpoint
             
             return await Task.FromResult(TypedResults.Ok<List<Todo>>(todos));
         }
+
+        // public static async Task<IResult> GetTodoById(int id)
+
+        // {
+        //     var todos = new List<Todo>()
+        //     {
+        //         new Todo { Id = 1, Title = "Do this", Description = "Do this thing", IsDone = false },
+        //         new Todo { Id = 2, Title = "Do that", Description = "Do that thing", IsDone = false },
+        //         new Todo { Id = 3, Title = "Do the other", Description = "Do the other thing", IsDone = false },
+        //     };
+
+        //     var todo = todos.FirstOrDefault(t => t.Id == id);
+
+        //     if (todo is null)
+        //     {
+        //         return await Task.FromResult(TypedResults.NotFound());
+        //     }
+
+        //     return await Task.FromResult(TypedResults.Ok(todo));
+        // }
 }
