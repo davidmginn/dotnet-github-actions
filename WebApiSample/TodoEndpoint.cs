778a5daa943037c8642e6f99ea65c7867edd190d
David Ginn <davidmginn@gmail.com>
2023-10-24 15:29:32 -0400

    Add get todo by id endpoint

--- WebApiSample/TodoEndpoint.cs
+++ WebApiSample/TodoEndpoint.cs
@@ -8,7 +8,7 @@ public static class TodoEndpoint
     public static RouteGroupBuilder MapTodoApi(this RouteGroupBuilder routeGroupBuilder)
     {
         routeGroupBuilder.MapGet("/", GetAllTodos);
-        //routeGroupBuilder.MapGet("/{id:int}", GetTodoById);
+        routeGroupBuilder.MapGet("/{id:int}", GetTodoById);
 
         return routeGroupBuilder;
     }
@@ -25,23 +25,23 @@ public static class TodoEndpoint
             return await Task.FromResult(TypedResults.Ok<List<Todo>>(todos));
         }
 
-        // public static async Task<IResult> GetTodoById(int id)
+        public static async Task<IResult> GetTodoById(int id)
 
-        // {
-        //     var todos = new List<Todo>()
-        //     {
-        //         new Todo { Id = 1, Title = "Do this", Description = "Do this thing", IsDone = false },
-        //         new Todo { Id = 2, Title = "Do that", Description = "Do that thing", IsDone = false },
-        //         new Todo { Id = 3, Title = "Do the other", Description = "Do the other thing", IsDone = false },
-        //     };
+        {
+            var todos = new List<Todo>()
+            {
+                new Todo { Id = 1, Title = "Do this", Description = "Do this thing", IsDone = false },
+                new Todo { Id = 2, Title = "Do that", Description = "Do that thing", IsDone = false },
+                new Todo { Id = 3, Title = "Do the other", Description = "Do the other thing", IsDone = false },
+            };
 
-        //     var todo = todos.FirstOrDefault(t => t.Id == id);
+            var todo = todos.FirstOrDefault(t => t.Id == id);
 
-        //     if (todo is null)
-        //     {
-        //         return await Task.FromResult(TypedResults.NotFound());
-        //     }
+            if (todo is null)
+            {
+                return await Task.FromResult(TypedResults.NotFound());
+            }
 
-        //     return await Task.FromResult(TypedResults.Ok(todo));
-        // }
+            return await Task.FromResult(TypedResults.Ok(todo));
+        }
 }
